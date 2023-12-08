3b6833144480a7ea1720beb06546f7edd2bb2ac4
Scorpion-CC <carloscantom@gmail.com>
2023-12-08 09:29:04 -0600

    Creación del archivo para la actividad.

---
+++ Canto_Cano_ActividadOrdinario/Canto_Cano_ActividadOrdinario/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Canto_Cano_ActividadOrdinario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
