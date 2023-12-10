0bcc7719931d526c7d0a6060f75c3547313e3783
Scorpion-CC <carloscantom@gmail.com>
2023-12-10 12:10:19 -0600

    Añadir las interfaces y enumeradores utilizadas.

---
+++ Canto_Cano_ActividadOrdinario/Canto_Cano_ActividadOrdinario/Interfaces/ICarta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Canto_Cano_ActividadOrdinario.Enumeradores;
+
+namespace Canto_Cano_ActividadOrdinario.Interfaces
+{
+    internal interface ICarta
+    {
+        FigurasCartasEnum Figura { get; }
+        ValoresCartasEnum Valor { get; }
+    }
+}
---
+++ Canto_Cano_ActividadOrdinario/Canto_Cano_ActividadOrdinario/Interfaces/IJuego.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Canto_Cano_ActividadOrdinario.Interfaces
+{
+    public interface IJuego
+    {
+        IDealer Dealer { get; }
+        bool JuegoTerminado { get; }
+        void AgregarJugador(IJugador jugador);
+        void IniciarJuego();
+        void JugarRonda();
+        void MostrarGanador();
+    }
+}
