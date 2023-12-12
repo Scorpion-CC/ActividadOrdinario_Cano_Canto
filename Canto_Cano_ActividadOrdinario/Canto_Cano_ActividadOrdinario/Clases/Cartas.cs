96c3aca4d7db3bb7ecd53cbf1f52ecefa5ccfc86
Scorpion-CC <carloscantom@gmail.com>
2023-12-12 14:47:47 -0600

    Añadir una interfaz a la clase Cartas
    Se añadió la interfaz IDeckDeCartas a la clase cartas para que esta tenga todas las funciones que brinda esta interfaz.

--- Canto_Cano_ActividadOrdinario/Canto_Cano_ActividadOrdinario/Clases/Cartas.cs
+++ Canto_Cano_ActividadOrdinario/Canto_Cano_ActividadOrdinario/Clases/Cartas.cs
@@ -9,10 +9,35 @@ using Canto_Cano_ActividadOrdinario.Clases;
 
 namespace Canto_Cano_ActividadOrdinario.Clases
 {
-    public class Cartas : ICarta
+    public class Cartas : ICarta , IDeckDeCartas
     {
         public FigurasCartasEnum Figura => throw new NotImplementedException();
 
         public ValoresCartasEnum Valor => throw new NotImplementedException();
+
+        public void BarajearDeck()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MeterCarta(ICarta carta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void MeterCarta(List<ICarta> cartas)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICarta SacarCarta(int indiceCarta)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICarta VerCarta(int indiceCarta)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
