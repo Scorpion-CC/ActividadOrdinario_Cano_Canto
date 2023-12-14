5dfa4d46d0d818db548ba0a86dc8130d175d7f05
Scorpion-CC <carloscantom@gmail.com>
2023-12-14 13:32:14 -0600

    Desarrollo de la función IniciarJuego en la clase Poker.
    Se desarrolló el comportamiento de la función para que funcione de acuerdo a las reglas del Poker clásico.

--- Canto_Cano_ActividadOrdinario/Canto_Cano_ActividadOrdinario/Clases/Poker.cs
+++ Canto_Cano_ActividadOrdinario/Canto_Cano_ActividadOrdinario/Clases/Poker.cs
@@ -29,48 +29,57 @@ namespace Canto_Cano_ActividadOrdinario.Clases
             for (int i = 0; i < Jugadores.Count; i++) 
             {
 
-                for (int j = 0; j < 5; j++)
-                {
                     int seleccion, numCarta, seleccion2;
                     bool JugadorQuiereCartas = false;
                     List<ICarta> cartasADevolver = new List<ICarta>();
 
-                    Jugadores[i].ObtenerCartas(Dealer.RepartirCartas(5));
-                    do
-                    {
-                        Console.WriteLine("¿Desea cambiar alguna carta? \n1)Si 2)Cambiar todas las cartas 3)No");
+                Console.WriteLine($"Baraja del jugador[{i+1}]:");
+                Jugadores[i].ObtenerCartas(Dealer.RepartirCartas(5));
+
+                do
+                {
+                        Console.WriteLine("\n¿Desea cambiar alguna carta? \n1)Si 2)Cambiar todas las cartas 3)No");
                         seleccion = rand.Next(1, 4); //Para que el bot seleccione un numero al azar del 1 al 2.
 
-                        Console.WriteLine($"Jugador[{i}]: {seleccion}"); //Ya que los jugadores son bots, tenemos que mostrar sus acciones de esta manera.
+                        Console.WriteLine($"Jugador[{i+1}]: {seleccion}"); //Ya que los jugadores son bots, tenemos que mostrar sus acciones de esta manera.
 
-                        if (seleccion == 1)
+                        if (seleccion == 3)
                         {
-
-                            Console.WriteLine("Elija la carta que quiera cambiar:");
-                            numCarta = rand.Next(0, 5);
-                            Console.WriteLine($"Jugador[{i}]: {numCarta + 1}");
-                            cartasADevolver.Add(Jugadores[i].DevolverCarta(numCarta)); //Modificar la clase jugador para que se elimine la carta de este antes de darla.
-                            Dealer.RecogerCartas(cartasADevolver);
-                            cartasADevolver.Clear();
-                            Jugadores[i].ObtenerCartas(Dealer.RepartirCartas(1));
+                            Console.ReadKey();
+                            Console.Clear();
+                            break;
                         }
                         else if (seleccion == 2)
                         {
                             Dealer.RecogerCartas(Jugadores[i].DevolverTodasLasCartas()); //Modificar el comportamiento de la funcion para eliminar las cartas despues de darlas.
                             Jugadores[i].ObtenerCartas(Dealer.RepartirCartas(5)); //Modificar el RepartirCartas para que imprima las cartas dadas.
+                            Console.WriteLine("Nueva baraja: ");
+                            Jugadores[i].MostrarCartas();//Aquí el usuario mostraría sus cartas.
                         }
-                        else { }
-                        Console.WriteLine("¿Quiere cambiar otra carta? \n 1)Si 2)No");
+                        else if (seleccion == 1)
+                        {
+
+                            Console.WriteLine("\nElija la carta que quiera cambiar:");
+                            numCarta = rand.Next(0, 5);
+                            Console.WriteLine($"Jugador[{i + 1}]: {numCarta + 1}");
+                            cartasADevolver.Add(Jugadores[i].DevolverCarta(numCarta)); //Modificar la clase jugador para que se elimine la carta de este antes de darla.
+                            Dealer.RecogerCartas(cartasADevolver);
+                            cartasADevolver.Clear();
+                            Jugadores[i].ObtenerCartas(Dealer.RepartirCartas(1));
+
+                        }
+                        Console.WriteLine("\n¿Quiere cambiar otra carta? \n 1)Si 2)No");
                         seleccion2 = rand.Next(1, 3);
-                        Console.WriteLine($"Jugador[{i}]: {seleccion2}");
+                        Console.WriteLine($"Jugador[{i+1}]: {seleccion2}");
                         if (seleccion2 == 1)
                         {
                             JugadorQuiereCartas = true;
                         }
-                        else { JugadorQuiereCartas = false; }
-
-                    } while (JugadorQuiereCartas);
-                }
+                        else if (seleccion2 == 2){JugadorQuiereCartas = false;}
+                        Console.ReadKey();
+                    Console.Clear();
+                } while (JugadorQuiereCartas == true);
+                    
             }
         }
 
