6d889df8c171276ee98ecd4d3e2cca7e75134f38
Scorpion-CC <carloscantom@gmail.com>
2023-12-14 13:06:11 -0600

    Implementar que se impriman las acciones de los bots.
    Se implementaron varios Console.WriteLine(); para que se pudiera ver en la consola las elecciones que tomaron los bots.

--- Canto_Cano_ActividadOrdinario/Canto_Cano_ActividadOrdinario/Clases/Poker.cs
+++ Canto_Cano_ActividadOrdinario/Canto_Cano_ActividadOrdinario/Clases/Poker.cs
@@ -40,11 +40,15 @@ namespace Canto_Cano_ActividadOrdinario.Clases
                     {
                         Console.WriteLine("¿Desea cambiar alguna carta? \n1)Si 2)Cambiar todas las cartas 3)No");
                         seleccion = rand.Next(1, 4); //Para que el bot seleccione un numero al azar del 1 al 2.
+
+                        Console.WriteLine($"Jugador[{i}]: {seleccion}"); //Ya que los jugadores son bots, tenemos que mostrar sus acciones de esta manera.
+
                         if (seleccion == 1)
                         {
 
                             Console.WriteLine("Elija la carta que quiera cambiar:");
-                            numCarta = rand.Next(1, 6);
+                            numCarta = rand.Next(0, 5);
+                            Console.WriteLine($"Jugador[{i}]: {numCarta + 1}");
                             cartasADevolver.Add(Jugadores[i].DevolverCarta(numCarta)); //Modificar la clase jugador para que se elimine la carta de este antes de darla.
                             Dealer.RecogerCartas(cartasADevolver);
                             cartasADevolver.Clear();
@@ -53,11 +57,12 @@ namespace Canto_Cano_ActividadOrdinario.Clases
                         else if (seleccion == 2)
                         {
                             Dealer.RecogerCartas(Jugadores[i].DevolverTodasLasCartas()); //Modificar el comportamiento de la funcion para eliminar las cartas despues de darlas.
-                            Jugadores[i].ObtenerCartas(Dealer.RepartirCartas(5));
+                            Jugadores[i].ObtenerCartas(Dealer.RepartirCartas(5)); //Modificar el RepartirCartas para que imprima las cartas dadas.
                         }
                         else { }
                         Console.WriteLine("¿Quiere cambiar otra carta? \n 1)Si 2)No");
                         seleccion2 = rand.Next(1, 3);
+                        Console.WriteLine($"Jugador[{i}]: {seleccion2}");
                         if (seleccion2 == 1)
                         {
                             JugadorQuiereCartas = true;
