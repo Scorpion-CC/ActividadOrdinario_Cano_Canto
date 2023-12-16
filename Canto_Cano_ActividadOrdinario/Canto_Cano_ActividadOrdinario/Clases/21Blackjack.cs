4c66c0279d1e374eea2c389bcb000b6b498ee3a6
Scorpion-CC <carloscantom@gmail.com>
2023-12-16 15:14:31 -0600

    Desarrollo de la función JugarRonda
    Se inció el desarrollo del comportamiento de la función IniciarRonda en la clase _21Blackjack y se finalizó el comportamiento para los turnos del jugador.

--- Canto_Cano_ActividadOrdinario/Canto_Cano_ActividadOrdinario/Clases/21Blackjack.cs
+++ Canto_Cano_ActividadOrdinario/Canto_Cano_ActividadOrdinario/Clases/21Blackjack.cs
@@ -39,6 +39,7 @@ namespace Canto_Cano_ActividadOrdinario.Clases
                 Jugadores[i].MostrarCartas();
                 
             }
+            Puntos.Add(0);
         }
 
         //CC: Ahora, después de iniciar el juego se le va a preguntar a cada uno si quiere una carta más, y así va a ser hasta que ya no quiera otra carta
@@ -46,7 +47,58 @@ namespace Canto_Cano_ActividadOrdinario.Clases
         //hay que guardar los puntos de cada uno en la lista Puntos que creamos. Al final de la ronda hay que crear un jugador más, que va a ser el dealer.
         public void JugarRonda() 
         {
-           
+            Console.ReadKey();
+            Console.Clear();
+            int seleccion, seleccion2;
+            for (int i = 0; i < Jugadores.Count; i++)
+            {
+                Console.WriteLine($"Turno del jugador[{i+1}]: \nDeck:");
+                List<ICarta> deckTemp = new List<ICarta>();
+                deckTemp = Jugadores[i].MostrarCartas();
+                for (int j = 0; j < Jugadores.Count; j++) //Esto es para calcular la puntuación actual que tiene cada jugador.
+                {
+                    if ((int)deckTemp[i].Valor == 1)
+                    {
+                        Console.WriteLine($"\nJugador[{i+1}], elije el valor del As en tu mano \n 1) 1pt  2) 11pts");
+                        if (Puntos[i] < 11)
+                        {
+                            seleccion2 = 2;
+                            Console.WriteLine($"\nJugador[{i + 1}]: {seleccion2}");
+                            Puntos[i] += 11;
+                        }
+                        else 
+                        {
+                            seleccion2 = 1;
+                            Console.WriteLine($"\nJugador[{i + 1}]: {seleccion2}");
+                            Puntos[i] += 1;
+                        }
+                    }
+                    else
+                    {
+                        Puntos[i] += (int)deckTemp[i].Valor; 
+                    }
+
+                }
+                do
+                {
+                    Console.WriteLine($"Puntos actuales (Jugador[{i + 1}]): {Puntos[i]}");
+                    Console.WriteLine($"Jugador[{i + 1}], ¿quieres otra carta? \n 1)Si  2)No");
+                    if (Puntos[i] < 18)
+                    {
+                        seleccion = 1;
+                        Console.WriteLine($"Jugador[{i + 1}]: {seleccion}");
+                        deckTemp.AddRange(Dealer.RepartirCartas(1));
+                        Puntos[i] = +(int)deckTemp[deckTemp.Count].Valor;
+                    }
+                    else
+                    {
+                        seleccion = 2;
+                        Console.WriteLine($"Jugador[{i + 1}]: {seleccion}");
+                    }
+                    Console.ReadKey();
+                    Console.Clear();
+                } while (Puntos[i] < 18);
+            }
         }
 
         //CC: En base al dealer verificamos quién gana y quién pierde, si este tiene más de 21 todos ganan (menos los que tengan más de 21). 
